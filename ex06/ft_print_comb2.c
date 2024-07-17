969ea1d3a069dc8e84b826f6d8309e9c35ec0520
liulm <lionelulm67@gmail.com>
2024-07-17 16:43:55 +0200

    started c00

--- ex06/ft_print_comb2.c
+++ ex06/ft_print_comb2.c
@@ -1,51 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_print_comb.c                                    :+:      :+:    :+:   */
+/*   ft_print_comb2.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: liulm <liulm@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/07/16 14:41:21 by liulm             #+#    #+#             */
-/*   Updated: 2024/07/17 13:49:01 by liulm            ###   ########.fr       */
+/*   Created: 2024/07/17 15:20:42 by liulm             #+#    #+#             */
+/*   Updated: 2024/07/17 15:21:08 by liulm            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void	ft_putchar(char c)
+void	ft_print_comb2(void)
 {
-	write(1, &c, 1);
-}
-
-void	ft_print_comb(void)
-{
-	int	a;
-	int	b;
-	int	c;
-
-	a = '0';
-	while (a <= '7')
-	{
-		b = a + 1;
-		while (b <= '8')
-		{
-			c = b + 1;
-			while (c <= '9')
-			{
-				ft_putchar(a);
-				ft_putchar(b);
-				ft_putchar(c);
-				if (a != '7' || b != '8' || c != '9')
-					write(1, ", ", 2);
-				c++;
-			}
-			b++;
-		}
-		a++;
-	}
-}
-
-int	main()
-{
-	ft_print_comb();
+	
 }
